00bd4d7cd8671466f092cd9e7496d22122354f04
Zhulduz Zhankenova <zhankenova@gmail.com>
2017-10-23 10:09:22 +0600

    Add standard linter

--- spec/autocomplete-ramda-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AutocompleteRamdaView from '../lib/autocomplete-ramda-view';
-
-describe('AutocompleteRamdaView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
