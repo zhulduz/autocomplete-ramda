ca9afeda4cee6bf4d72032265482fb278dc7a7e5
Zhulduz Zhankenova <zhankenova@gmail.com>
2017-10-23 09:47:20 +0600

    Add ramda autocomplete

---
+++ spec/autocomplete-ramda-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompleteRamdaView from '../lib/autocomplete-ramda-view';
+
+describe('AutocompleteRamdaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
